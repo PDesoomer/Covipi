4361dc2fcdb09b12f2903b15d9c6ece60db14ba7
= <p.desoomer@gmail.com>
2020-12-17 16:55:25 +0100

    Cleaning up the project by removing unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,8 @@ import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
 import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 import {
   AgmCoreModule
 } from '@agm/core';
@@ -35,7 +30,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     RouterModule,
     AppRoutingModule,
     AgmCoreModule.forRoot({
-      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+      apiKey: 'AIzaSyBKbbUnN1dQNaHNTyQhnyt7sqTJDVQaxBA'
     })
   ],
   declarations: [
--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -4,13 +4,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
+
 import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
 import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatRippleModule} from '@angular/material/core';
@@ -33,13 +29,8 @@ import {MatSelectModule} from '@angular/material/select';
   ],
   declarations: [
     DashboardComponent,
-    UserProfileComponent,
     TableListComponent,
-    TypographyComponent,
-    IconsComponent,
     MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent,
   ]
 })
 
